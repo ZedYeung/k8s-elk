297ccd5aa06be8118de0cf332a0a08fabee7fae1
Yue (Zed) Yang <shoominga@gmail.com>
2018-08-21 08:42:06 -0700

    add filebeat in run and cleanup sh

--- cleanup.sh
+++ cleanup.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
+kubectl delete -f ./storage_class
+
 kubectl delete -f ./00-namespace.yml
 kubectl delete -f ./rbac-namespace-default/
 
 kubectl delete -f ./elasticsearch/
-kubectl delete -f ./logstash/
+
 kubectl delete -f ./kibana/
+
+kubectl delete -f ./logstash
+kubectl delete -f ./filebeat
 
 kubectl delete -f ./localhost-services/
--- run.sh
+++ run.sh
@@ -2,12 +2,16 @@
 sudo mkdir -p /var/lib/docker/logstash/pipeline
 cp -r ./logstash/pipeline/. /var/lib/docker/logstash/pipeline
 
+kubectl apply -f ./storage_class
+
 kubectl apply -f ./00-namespace.yml
 kubectl apply -f ./rbac-namespace-default/
 
 kubectl apply -f ./elasticsearch/
-kubectl apply -f ./logstash/
 kubectl apply -f ./kibana/
+
+kubectl apply -f ./logstash/
+kubectl apply -f ./filebeat/
 
 kubectl apply -f ./localhost-services/
 echo $(minikube ip)

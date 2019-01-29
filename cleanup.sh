d7faa41ab72cb496b82c48a19dc80bfccf79da02
Yue (Zed) Yang <shoominga@gmail.com>
2019-01-29 08:43:31 +0800

    update for gke

--- cleanup.sh
+++ cleanup.sh
@@ -2,13 +2,9 @@
 kubectl delete -f ./storage_class
 
 kubectl delete -f ./00-namespace.yml
-kubectl delete -f ./rbac-namespace-default/
 
 kubectl delete -f ./elasticsearch/
-
 kubectl delete -f ./kibana/
 
 kubectl delete -f ./logstash
 kubectl delete -f ./filebeat
-
-kubectl delete -f ./localhost-services/

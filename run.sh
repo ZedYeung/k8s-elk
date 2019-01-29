82042917922a2d88cde93cb8404130c9082e2b1c
Yue (Zed) Yang <shoominga@gmail.com>
2019-01-29 08:41:54 +0800

    config for gke

--- run.sh
+++ run.sh
@@ -5,13 +5,9 @@ sudo cp -r ./logstash/pipeline /usr/share/logstash/
 kubectl apply -f ./storage_class
 
 kubectl apply -f ./00-namespace.yml
-kubectl apply -f ./rbac-namespace-default/
 
 kubectl apply -f ./elasticsearch/
 kubectl apply -f ./kibana/
 
 kubectl apply -f ./logstash/
 kubectl apply -f ./filebeat/
-
-kubectl apply -f ./localhost-services/
-# echo $(minikube ip)
